94133e93c1ab1a3b96035cd7f9f8e653cff90d8a
Thomas Schibler <tschibler@gmail.com>
2024-11-19 09:52:51 -0800

    Create scheduling_alg submodule and refactor main accordingly

--- src/main.py
+++ src/main.py
@@ -7,51 +7,7 @@ import networkx as nx
 from draw_graph import draw_dag_topological
 from longest_paths import longest_path_lengths
 from random_graph import generate_random_dag 
-
-def schedule_lrp(weighted_dag):
-    lengths = longest_path_lengths(dag)
-    priority_list = sorted(lengths.keys(), key=lambda k: lengths[k])
-    t = 0
-    nodes = weighted_dag.nodes
-    remaining_degree = {}
-    not_ready = set()
-    ready = []
-    for n in nodes:
-        remaining_degree[n] = 0
-
-    for edge in weighted_dag.edges:
-        remaining_degree[edge[1]] += 1
-    for n in nodes:
-        if remaining_degree[n] > 0:
-            not_ready.add(n)
-        else:
-            heapq.heappush(ready, (0, n))
-    schedule = {}
-    event_queue = [(0, None)]
-    while len(schedule) < len(nodes):
-        event = heapq.heappop(event_queue)
-        t, n = event
-        # Time step
-        if n is None:
-            if len(ready) > 0:
-                next_node = heapq.heappop(ready)[1]
-                schedule[t] = next_node
-                for edge in weighted_dag.out_edges(next_node, data=True):
-                    # Edge from a to b is in form (a, b, {'weight': w})
-                    b = edge[1]
-                    w = edge[2]['weight']
-                    heapq.heappush(event_queue, (t + w - 0.5, b))
-                    
-            heapq.heappush(event_queue, (t+1, None))
-        # Dependency resolved for node n
-        else:
-            remaining_degree[n] -= 1
-            if remaining_degree[n] == 0:
-                not_ready.remove(n)
-                heapq.heappush(ready, (-1 * lengths[n], n))
-    return schedule
-
-
+from scheduling_algs.lrp_scheduling_alg import lrp_scheduling_alg
 
 debug = True
 iterations = 1
@@ -81,8 +37,9 @@ for _ in range(iterations):
     
     if debug:
         print(f'lengths: {lengths}')
-    
-    schedule = schedule_lrp(dag)
+
+    alg = lrp_scheduling_alg()
+    schedule = alg.schedule(dag, 1)
     schedule_time = max(schedule.keys())
     ratio = schedule_time / float(lower_bound)
     
