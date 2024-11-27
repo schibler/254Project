9338013f9b3b01d43674e5aeaa15730e1a2df073
Thomas Schibler <tschibler@gmail.com>
2024-11-26 16:36:19 -0800

    Move details of running to new experiment.py

--- src/main.py
+++ src/main.py
@@ -5,49 +5,22 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 from draw_graph import draw_dag_topological
+from experiment import experiment, experiment_result
 from longest_paths import longest_path_lengths
 from random_graph import generate_random_dags 
 from scheduling_algs.lrp_scheduling_alg import lrp_scheduling_alg
 
 debug = True
-iterations = 1
+iterations = 1000
 ratios = []
 
 # Generate a set of graphs
 dags = generate_random_dags(iterations, n=10, p=0.3, t=4)
+alg = lrp_scheduling_alg()
+num_fus = 1
 
-for dag in dags:
-    # Print the generated edges with weights
-    if debug:
-        for u, v, weight in dag.edges(data='weight'):
-            print(f"Edge from {u} to {v} with weight {weight}")
-    
-    lengths = longest_path_lengths(dag)
-    
-    # Easy lower bounds:
-    #     n: need to schedule every graph node
-    #     longest path: need to respect latency precedence constraints
-    # Take max of the two
-    length_lower_bound = max([lengths[key] for key in lengths])
-    lower_bound = max(length_lower_bound, dag.number_of_nodes())
-    
-    if debug:
-        print(f'lengths: {lengths}')
-
-    alg = lrp_scheduling_alg()
-    schedule = alg.schedule(dag, 1)
-    schedule_time = max(schedule.keys())
-    ratio = schedule_time / float(lower_bound)
-    
-    if debug:
-        print(f'schedule: {schedule}')
-        print(f"schedule time = {schedule_time}")
-        print(f"lower bound = {lower_bound}")
-        print(f"ratio = {ratio}\n")
-        draw_dag_topological(dag)
-    ratios.append(ratio)
-
-ratios = sorted(ratios)
-print(f"max ratio = {max(ratios)}")
-print(f"min ratio = {min(ratios)}")
-print(f"median ratio = {ratios[int(len(ratios)/2)]}")
+exp = experiment(dags, alg, num_fus)
+result = exp.run()
+print(f"max ratio = {result.max_ratio()}")
+print(f"min ratio = {result.min_ratio()}")
+print(f"median ratio = {result.median_ratio()}")
--- src/scheduling_algs/lrp_scheduling_alg.py
+++ src/scheduling_algs/lrp_scheduling_alg.py
@@ -6,9 +6,8 @@ from longest_paths import longest_path_lengths
 
 class lrp_scheduling_alg:
 
-    @classmethod
-    def schedule(cls, dag: nx.DiGraph, num_processors: int) -> dict[int, int]:
-        # TODO: leverage num_processors
+    def schedule(self, dag: nx.DiGraph, num_fus: int) -> dict[int, int]:
+        # TODO: leverage num_fus
         lengths = longest_path_lengths(dag)
         priority_list = sorted(lengths.keys(), key=lambda k: lengths[k])
         t = 0
@@ -26,16 +25,16 @@ class lrp_scheduling_alg:
                 not_ready.add(n)
             else:
                 heapq.heappush(ready, (0, n))
-        schedule = {}
+        ret_schedule = {}
         event_queue = [(0, None)]
-        while len(schedule) < len(nodes):
+        while len(ret_schedule) < len(nodes):
             event = heapq.heappop(event_queue)
             t, n = event
             # Time step
             if n is None:
                 if len(ready) > 0:
                     next_node = heapq.heappop(ready)[1]
-                    schedule[t] = next_node
+                    ret_schedule[t] = next_node
                     for edge in dag.out_edges(next_node, data=True):
                         # Edge from a to b is in form (a, b, {'weight': w})
                         b = edge[1]
@@ -49,4 +48,4 @@ class lrp_scheduling_alg:
                 if remaining_degree[n] == 0:
                     not_ready.remove(n)
                     heapq.heappush(ready, (-1 * lengths[n], n))
-        return schedule
+        return ret_schedule
